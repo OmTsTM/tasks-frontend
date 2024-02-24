d90b64f9b9c69423a0bc5de44505b2c9766f363c
MATTEUS TEKESHI DIAS <matteus_mindlabs@getnet.com.br>
2024-02-23 22:08:22 -0300

    Porta

--- src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
@@ -5,17 +5,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import br.ce.wcaquino.tasksfrontend.model.Todo;
 
 @Controller
-public class TasksController {
+public class TasksController implements ErrorController {
+
+	private final static String PATH = "/error";
+    @Override
+    @RequestMapping(PATH)
+    @ResponseBody
+    public String getErrorPath() {
+        // TODO Auto-generated method stub
+        return "No Mapping Found";
+    }
 	
 	@Value("${backend.host}")
 	private String BACKEND_HOST;
